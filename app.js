00931e085f5c856855448c1b94bbf31634ffb836
Harsh Gupta <harshgupta6641@gmail.com>
2020-02-11 17:44:10 +0000

    index

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express =require("express"),
-    app=express();
+    app=express(),
+    bodyParser=require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
+app.use(express.static(__dirname + "/public"));
+
 
 app.get("/",function(req,res){
    res.render("index");
